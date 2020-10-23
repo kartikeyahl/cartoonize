a292687d529cf6f30b3d149f86be7407825ad4ba
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-23 07:53:49 +0530

    Update cartooify.py

--- back-end/cartooify.py
+++ back-end/cartooify.py
@@ -1,28 +1,67 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf 
+import network
+import guided_filter
+from tqdm import tqdm
 
-#1) importing libraries
-import cv2                 # importing opencv library (image recog., image preprocessing)
-import numpy as np         # importing numpy(to solve mathematical expression)
 
-#2) taking image 
-img = cv2.imread("rat.jpg")
+def resize_crop(image):
+    h, w, c = np.shape(image)
+    if min(h, w) > 720:
+        if h > w:
+            h, w = int(720*h/w), 720
+        else:
+            h, w = 720, int(720*w/h)
+    image = cv2.resize(image, (w, h),
+                       interpolation=cv2.INTER_AREA)
+    h, w = (h//8)*8, (w//8)*8
+    image = image[:h, :w, :]
+    return image
+    
 
-# 3) Edges
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    #converting to gray-scale
-gray = cv2.medianBlur(gray, 5)                  #giving blur
-edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)   #defining image borders
+def cartoonize(load_folder, save_folder, model_path):
+    input_photo = tf.placeholder(tf.float32, [1, None, None, 3])
+    network_out = network.unet_generator(input_photo)
+    final_out = guided_filter.guided_filter(input_photo, network_out, r=1, eps=5e-3)
 
-# 4) Color
-color = cv2.bilateralFilter(img, 9, 300, 300)       # assighning weights
+    all_vars = tf.trainable_variables()
+    gene_vars = [var for var in all_vars if 'generator' in var.name]
+    saver = tf.train.Saver(var_list=gene_vars)
+    
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
 
-# 5) Cartoon
-cartoon = cv2.bitwise_and(color, color, mask=edges)  #image masking(applying algo for cartoonifying)
+    sess.run(tf.global_variables_initializer())
+    saver.restore(sess, tf.train.latest_checkpoint(model_path))
+    name_list = os.listdir(load_folder)
+    for name in tqdm(name_list):
+        try:
+            load_path = os.path.join(load_folder, name)
+            save_path = os.path.join(save_folder, name)
+            image = cv2.imread(load_path)
+            image = resize_crop(image)
+            batch_image = image.astype(np.float32)/127.5 - 1
+            batch_image = np.expand_dims(batch_image, axis=0)
+            output = sess.run(final_out, feed_dict={input_photo: batch_image})
+            output = (np.squeeze(output)+1)*127.5
+            output = np.clip(output, 0, 255).astype(np.uint8)
+            cv2.imwrite(save_path, output)
+        except:
+            print('cartoonize {} failed'.format(load_path))
 
-# 6) display
-cv2.imshow("Image", img)
-#cv2. imshow("Edges", edges)
-#cv2. imshow("color", color)
-cv2.imshow("Cartoon", cartoon)
-cv2.imwrite("rat1.jpg",cartoon)              #saving cartoon image in same folder
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
+    
+
+if __name__ == '__main__':
+    model_path = 'saved_models'
+    load_folder = 'test_images'
+    save_folder = 'cartoonized_images'
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+    cartoonize(load_folder, save_folder, model_path)
+    
+
+    
